0c99a3147e0c4a5cf9340264d6160521b5d07332
wtka <knife44978@gmail.com>
2022-03-06 22:26:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <img src="https://picsum.photos/300/200?random=10" class="w-100"> -->
+  <h1>你好</h1>
+  <i class="bi bi-alarm-fill"></i>
+  <button class="btn-primary">按我</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+@import "./assets/stylesheet/all"
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import App from './App.vue'
+
+createApp(App).mount('#app')
